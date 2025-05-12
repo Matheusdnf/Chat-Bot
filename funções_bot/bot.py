833315201fe21efdf8a5d95665bbde29c88c0ac1
Matheusdnf <matheusdiniz870@gmail.com>
2025-05-12 20:25:16 -0300

    feat - funcionalidade bot

--- funções_bot/bot.py
+++ funções_bot/bot.py
@@ -7,14 +7,15 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import requests
 from bs4 import BeautifulSoup
-
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 def Bot (memoria,chat):
-    comportamento_chat=[SystemMessage("Me trate como se eu fosse seu pai")]
+    comportamento_chat=[SystemMessage("Você é um agente ia que irá responder dúvidas sobre o curso de bsi, seja o mais formal,e só responda as perguntas baseadas no pdf carregado, não busque na sua base de dados")]
     memoria+=comportamento_chat
     template = ChatPromptTemplate.from_messages (memoria)
-
     chain = template | chat
+
     try:
         return chain.invoke({}).content
     except APIStatusError as e:
@@ -30,55 +31,20 @@ def Bot (memoria,chat):
 
 
 
-def informação_de_sites(url_site, memoria):
-    print(f"Carregando a página: {url_site}")
-    response = requests.get(url_site)
+def informacao_pdf(memoria):
+    caminho = "./pdfs/"
     
-    # posso adicioanr um bloco try exception -> erro que dará requests.exceptions.HTTPError
-    try:
-        if (response.status_code == 200):
-            print(response)
-            content = response.text  
-            print(content)
-            print("Acesso a informação conseguida com sucesso.")
-    except requests.exceptions.HTTPError as e:
-            print("Erro na requizição {e}")
-    finally:
-        print("Falha ao fazer o carregamento da página")
 
-    # Parse com BeautifulSoup
-    soup = BeautifulSoup(content, 'html.parser')
+    loader = PyPDFDirectoryLoader(path=caminho)
+    conteudo_pdf = loader.load()
+    print(conteudo_pdf)
 
-    # Encontrando as tags <main> 
-    main_content = soup.find('main')  # Busca a tag <main>
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=9000, chunk_overlap=0)
+    
+    load_pdf = []
 
-    # Lista para armazenar o conteúdo extraído
-    info_site = []
+    for doc in conteudo_pdf:
+        chunks = text_splitter.split_text(doc.page_content)
+        load_pdf.extend(chunks)
 
-    # Função para limpar e extrair texto
-    def clean_text(tag):
-        # Remover as tags de imagem, vídeo, link, estilização e script
-        for unwanted_tag in tag.find_all(['img', 'video', 'a', 'style', 'script','footer']):
-            unwanted_tag.decompose()  # Remove a tag indesejada
-
-        # Remover atributos como 'class' e 'id' para não extrair estilização
-        for element in tag.find_all(True):  # True significa todas as tags
-            del element['class']
-            del element['id']
-
-        # Extrair e retornar o texto limpo
-        return tag.get_text(separator=" ", strip=True)
-
-    if main_content:
-        print("Conteúdo da tag <main> encontrado:")
-        info_site.append(clean_text(main_content))  # Extraí o texto limpo da tag <main>
-    else:
-        print("Tag <main> não encontrada.")
-
-
-    memoria += info_site
-
-    # print(f"\nConteúdo completo processado (apenas texto limpo):")
-    # for i, chunk in enumerate(info_site):  # Exibe todo o conteúdo processado
-    #     print(f"Texto Extraído (Chunk {i}):\n{chunk}\n")
-
+    memoria.extend(load_pdf)
