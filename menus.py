d5cb7a00f190158a1acf4c167691b13848e49095
Matheusdnf <matheusdiniz870@gmail.com>
2025-05-12 20:25:10 -0300

    feat - menu de navegação

--- menus.py
+++ menus.py
@@ -1,18 +1,16 @@
 from teste import *
 from groq import Groq
+
 Iniciar=True
 while Iniciar:
     print("Chat Bot Teste")
-    print("[1-Adicionar Informação de Site]")
+    print("[1-Adicionar PDF]")
     pergunta=input("Qual A sua Pergunta?")
+    
     if (pergunta.lower() == "x"):
         Iniciar=False
-    if (pergunta == "1"):
-        url_site=input("Qual link de site deseja adicionar a infomação?")
-        informação_de_sites(url_site,memoria)
-        print("Informação adicionada, agr faça uma pergunta sobre o que foi adicionado")
-        #print(memoria)
-
+    if (pergunta=="1"):
+        informacao_pdf(memoria)
     else:
         usuario_pergunta(memoria,pergunta)
         resposta = Bot(memoria,chat)
--- teste.py
+++ teste.py
@@ -2,14 +2,20 @@ import os
 #interação com as ia 
 from langchain_groq import ChatGroq
 
-from funções_bot.bot import Bot
+
+
+from funções_bot.bot import *
 from pergunta_usuario.usuario import usuario_pergunta
 
 memoria=[]
 
-api_key = "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"
+api_key = "chave_api"
 os.environ["GROQ_API_KEY"] = api_key
-chat = ChatGroq(model="llama-3.3-70b-versatile")
+chat = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    max_retries=3,
+    temperature=2
+)
 
 
 
